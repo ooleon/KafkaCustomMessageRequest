e911a433f76b2821bba1c59cd9beb9c2dc0db605
ooleon <ooleon@hotmail.com>
2023-11-22 19:58:38 -0300

    initial commit

---
+++ src/main/java/localhost/KafkaExm1Application.java
@@ -0,0 +1,13 @@
+package localhost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExm1Application.class, args);
+	}
+
+}
